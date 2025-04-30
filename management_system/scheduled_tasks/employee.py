493cc6d26a7f53cb804541fccc71351cf30f4410
Mahmoud Hussein <mahmoud.hussain@golive-solutions.com>
2025-04-30 22:01:59 +0000

    fix: Update employee data retrieval and calculation logic in scheduled tasks

--- management_system/scheduled_tasks/employee.py
+++ management_system/scheduled_tasks/employee.py
@@ -2,7 +2,7 @@ import frappe
 from datetime import datetime 
 
 def calculate_days():
-    employees = frappe.get_list("Employees",filters={"hired":1},fields={"name","hired_on"})
+    employees = frappe.get_list("MS Employee", fields=["name", "hired_on"])
     today = datetime.now().strftime('%Y-%m-%d')
     today = datetime.strptime(today, '%Y-%m-%d')
     for employee in employees:
@@ -11,5 +11,8 @@ def calculate_days():
             hired_on = hired_on.strftime('%Y-%m-%d')
             hired_on=datetime.strptime(hired_on, '%Y-%m-%d')
             number_of_days =( today - hired_on).days
-            frappe.db.set_value("Employees", employee.name, "days_employed", int(number_of_days)+1)
+            frappe.db.set_value("MS Employee", employee.name, "days_employed", int(number_of_days)+1)
+            frappe.db.commit()
+        else:
+            frappe.db.set_value("MS Employee", employee.name, "days_employed", 0)
             frappe.db.commit()
